4b4f4889487c186540e1ac48c234c6e1c2ef46db
Valera <valerij.bancer@gmail.com>
2014-03-16 01:07:51 +0100

    Test cases for the helper class.

--- Test/Case/View/Helper/JsTestHelperTest.php
+++ Test/Case/View/Helper/JsTestHelperTest.php
@@ -9,6 +9,10 @@ App::uses('JsTestHelper', 'JsTests.View/Helper');
  */
 class JsTestHelperTest extends CakeTestCase {
 
+	public static function setupBeforeClass() {
+		require_once(App::pluginPath('JsTests').'Test'.DS.'config.php');
+	}
+
 /**
  * setUp method
  *
@@ -30,14 +34,35 @@ class JsTestHelperTest extends CakeTestCase {
 
 		parent::tearDown();
 	}
+	
+	public function testTestLink() {
+		$this->JsTest->settings['url'] = Configure::read('JsTests.Profiles.default.url');
+		$result = $this->JsTest->testLink('Run', 'some-library.test.html');
+		$expected = $this->JsTest->Html->link('Run', 'js/tests/some-library.test.html');
+		$this->assertEqual($result, $expected);
+	}
 
 /**
  * testTestLink method
  *
  * @return void
  */
-	public function testTestLink() {
-		$this->markTestSkipped('testIndex not implemented.');
+	public function testTestLinkCakeWay() {
+		$this->JsTest->settings['url'] = Configure::read('JsTests.Profiles.cake_way.url');
+		$result = $this->JsTest->testLink('Run', 'test_example1');
+		$expected = $this->JsTest->Html->link('Run', 'js_tests/qunit_tests/test_example1');
+		$this->assertEqual($result, $expected);
+	}
+	
+	public function testCoverageLink() {
+		$this->JsTest->settings['url'] = Configure::read('JsTests.Profiles.default.url');
+		$result = $this->JsTest->coverageLink('Run', 'some-library.test.html');
+		$param = $this->JsTest->Html->url('js_instrumented/tests/some-library.test.html');
+		$expected = $this->JsTest->Html->link(
+			'Run',
+			'js_instrumented/jscoverage.html?u='.$param
+		);
+		$this->assertEqual($result, $expected);
 	}
 
 /**
@@ -45,8 +70,15 @@ class JsTestHelperTest extends CakeTestCase {
  *
  * @return void
  */
-	public function testCoverageLink() {
-		$this->markTestIncomplete('testIndex not implemented.');
+	public function testCoverageLinkCakeWay() {
+		$this->JsTest->settings['url'] = Configure::read('JsTests.Profiles.cake_way.url');
+		$result = $this->JsTest->coverageLink('Run', 'test_example1');
+		$param = $this->JsTest->Html->url('js_tests/qunit_tests/test_example1/coverage');
+		$expected = $this->JsTest->Html->link(
+			'Run',
+			'js_instrumented/jscoverage.html?u='.$param
+		);
+		$this->assertEqual($result, $expected);
 	}
 
 }
